968cdaa887c21a382936afc6304da3710fa95917
Taras-prog <tarascr7@gmail.com>
2020-07-29 23:09:12 +0300

    commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Header.css';
+
+const HeaderComponent = () => {
+return (<div>
+    <h1>Our travels <img className="logo" src="https://cdn0.iconfinder.com/data/icons/miscellaneous-51-line/128/tourism_touring_ramble_excursion_travel-512.png" alt=""></img>
+    </h1>
+    </div>)
+};
+
+export default HeaderComponent;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './Navbar.css';
+
+
+const Navbar = () => {
+    return (<div>
+
+
+<nav className="navbar navbar-dark bg-primary">
+<ul>
+  <li><a href="/"></a>Home</li>
+  <li><a href="/"></a>About us</li>
+</ul>
+</nav>
+
+
+<nav className="navbar navbar-light" >
+ 
+</nav>
+        
+        </div>)
+};
+
+export default Navbar;
